b1d54b20a1b85009b072482f7ac72cda139860cc
HeJingHui0 <q1036849399@163.com>
2020-07-11 22:48:04 +0800

    首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,47 @@
-//Page Object
+import { request } from "../../request/index.js"
 Page({
   data: {
-    
+    swiperList: [],
+    cateList: [],
+    floorList: []
   },
   //options(Object)
   onLoad: function(options) {
-    
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList: result.data.message
+    //     })
+    //   },
+    // });
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList()
+  },
+  getSwiperList() {
+    request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
+    .then(result => {
+      this.setData({ 
+        swiperList: result.data.message
+      })
+    })
+  },
+  getCateList() {
+    request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'})
+    .then(result => {
+      this.setData({ 
+        cateList: result.data.message
+      })
+    })
+  },
+  getFloorList() {
+    request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'})
+    .then(result => {
+      this.setData({ 
+        floorList: result.data.message
+      })
+    })
   },
   onReady: function() {
     
