96df5346418d25e45c9b0b4c0ba0180adb94f808
HeJingHui0 <q1036849399@163.com>
2020-07-14 20:32:02 +0800

    商品详情

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,20 +1,50 @@
-// pages/goods_detail/index.js
+import { request } from "../../request/index.js"
+import regeneratorRuntime from "../../lib/runtime/runtime"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goodsDetail: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  goodsImage: {},
   onLoad: function (options) {
-
+    const {goods_id} = options
+    this.getGoodsDetail(goods_id)
   },
-
+  async getGoodsDetail(goods_id) {
+    const result = await request({url: '/goods/detail', data: {goods_id}})
+    this.goodsImage = result
+    this.setData({
+      goodsDetail: {
+        goods_name: result.goods_name,
+        goods_price: result.goods_price,
+        goods_introduce: result.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: result.pics
+      }
+    })
+  },
+  handlePrevewImage(e) {
+    const urls = this.goodsImage.pics.map(item => item.pics_mid)
+    const current = e.currentTarget.dataset.url
+    wx.previewImage({
+      current,
+      urls
+    })
+  },
+  handleCartAdd() {
+    let cart = wx.getStorageSync('cart') || []
+    let index = cart.findIndex(item => item.goods_id === this.goodsImage.goods_id)
+    if(index === -1) {
+      this.goodsImage.num = 1
+      cart.push(this.goodsImage)
+    }else {
+      cart[index].num ++
+    }
+    wx.setStorageSync('cart', cart)
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      mask: true
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
