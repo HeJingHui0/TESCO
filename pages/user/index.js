57207c6223826ab95cb510244e004e2fd5588e42
HeJingHui0 <q1036849399@163.com>
2020-07-19 15:59:24 +0800

    个人中心

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,11 +1,13 @@
 // pages/user/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    userInfo: {}
+  },
+  onShow() {
+    const userInfo = wx.getStorageSync('userInfo')
+    this.setData({
+      userInfo
+    })
   },
 
   /**
@@ -19,13 +21,6 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
   },
 
